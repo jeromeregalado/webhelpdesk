b29a951993827fb9dd2ac6b28ac7e30a5d1d7c84
jeromeregalado <jregalado@exist.com>
2024-03-26 00:52:35 +0800

    Added spring security. Request matchers are still in progress

--- src/main/java/com/example/WebHelperDesk/controller/HelpdeskTicketController.java
+++ src/main/java/com/example/WebHelperDesk/controller/HelpdeskTicketController.java
@@ -1,14 +1,11 @@
 package com.example.WebHelperDesk.controller;
 
 import com.example.WebHelperDesk.dto.AssignRequest;
-import com.example.WebHelperDesk.dto.AssignedIdDTO;
 import com.example.WebHelperDesk.dto.DeleteTicketRequest;
 import com.example.WebHelperDesk.entity.ticketing.HelpdeskTicket;
 import com.example.WebHelperDesk.service.HelpdeskTicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/tickets")
--- src/main/java/com/example/WebHelperDesk/controller/WatcherController.java
+++ src/main/java/com/example/WebHelperDesk/controller/WatcherController.java
@@ -1,10 +1,12 @@
 package com.example.WebHelperDesk.controller;
 
 import com.example.WebHelperDesk.dto.WatcherRequest;
-import com.example.WebHelperDesk.entity.ticketing.HelpdeskTicket;
 import com.example.WebHelperDesk.service.WatcherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/watchers")
