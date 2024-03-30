30379fb49e5f81916aeedfa5a8d62a558f9e1975
jeromeregalado <jregalado@exist.com>
2024-03-31 06:03:21 +0800

    added cors configuration

--- src/main/java/com/example/WebHelperDesk/controller/EmployeeController.java
+++ src/main/java/com/example/WebHelperDesk/controller/EmployeeController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
+@CrossOrigin(origins = "http://localhost:4200/*")
 public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
